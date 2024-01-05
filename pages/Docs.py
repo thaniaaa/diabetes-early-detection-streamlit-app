45b9f8a3b187043495b6a7e8fc3a2a26a001db65
Thania Aprilah <aprilahthania@gmail.com>
2024-01-05 14:58:47 +0700

    update functions dan  docs

--- pages/Docs.py
+++ pages/Docs.py
@@ -14,22 +14,22 @@ df_display = decode(df)
 st.write(df_display)
 st.divider()
 
-attr_desc = pd.DataFrame([["age", "Lorem ipsum"],
-                           ["gender", "Aliquam vitae est non elit blandit dictum."],
-                           ["polyuria", "Aliquam vitae est non elit blandit dictum."],
-                           ["polydipsia", "Aliquam vitae est non elit blandit dictum."],
-                           ["sudden_weight_loss", "Aliquam vitae est non elit blandit dictum."],
-                           ["weakness", "Aliquam vitae est non elit blandit dictum."],
-                           ["polyphagia", "Aliquam vitae est non elit blandit dictum."],
-                           ["visual_blurring", "Aliquam vitae est non elit blandit dictum."],
-                           ["genital Thrush", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
-                           ["itching", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
-                           ["irritability", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
-                           ["delayed_Healing", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
-                           ["partial_paresis", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
-                           ["muscle_stiffness", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
-                           ["alopecia", "Aliquam vitae est non elit blandit dictum."],
-                           ["obesity", "Aliquam vitae est non elit blandit dictum."],
+attr_desc = pd.DataFrame([["age", "Usia"],
+                           ["gender", "Jenis kelamin"],
+                           ["polyuria", "Sering buang air kecil"],
+                           ["polydipsia", "Sering merasa haus"],
+                           ["sudden_weight_loss", "Penurunan berat badan secara tiba-tiba"],
+                           ["weakness", "Kelelahan"],
+                           ["polyphagia", "Sering merasa lapar"],
+                           ["visual_blurring", "Penglihatan kabur"],
+                           ["genital Thrush", "Infeksi jamur Candida pada area genital."],
+                           ["itching", "Gatal-gatal"],
+                           ["irritability", "Perasaan mudah marah dan tersinggung"],
+                           ["delayed_Healing", "Penyembuhan luka yang lama"],
+                           ["partial_paresis", "Kelumpuhan sebagian"],
+                           ["muscle_stiffness", "Kekakuan otot"],
+                           ["alopecia", "Rambut rontok"],
+                           ["obesity", "Berat badan berlebih"],
                            ], columns=["Atribut/fitur", "Deskripsi"])
 st.subheader("Deskripsi")
 st.table(attr_desc)
