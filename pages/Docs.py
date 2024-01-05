50e066a0fde5e826cebb126d88f6f205f07a01e4
yhfie <huwaidar5@gmail.com>
2024-01-05 14:13:15 +0700

    UX update yefo #1

--- pages/Docs.py
+++ pages/Docs.py
@@ -4,12 +4,33 @@ import numpy as np
 
 from functions import decode
 
-st.write("# Dokumentasi")
+st.header("Dokumentasi")
+st.divider()
 
 # Dataset
 df = pd.read_csv("diabetes_data_final.csv")
-st.write(" ## Dataset")
+st.subheader("Dataset")
 df_display = decode(df)
 st.write(df_display)
 st.divider()
+
+attr_desc = pd.DataFrame([["age", "Lorem ipsum"],
+                           ["gender", "Aliquam vitae est non elit blandit dictum."],
+                           ["polyuria", "Aliquam vitae est non elit blandit dictum."],
+                           ["polydipsia", "Aliquam vitae est non elit blandit dictum."],
+                           ["sudden_weight_loss", "Aliquam vitae est non elit blandit dictum."],
+                           ["weakness", "Aliquam vitae est non elit blandit dictum."],
+                           ["polyphagia", "Aliquam vitae est non elit blandit dictum."],
+                           ["visual_blurring", "Aliquam vitae est non elit blandit dictum."],
+                           ["genital Thrush", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
+                           ["itching", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
+                           ["irritability", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
+                           ["delayed_Healing", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
+                           ["partial_paresis", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
+                           ["muscle_stiffness", "Maecenas eu faucibus ex. Aenean vel sem congue ante posuere interdum et imperdiet ipsum.."],
+                           ["alopecia", "Aliquam vitae est non elit blandit dictum."],
+                           ["obesity", "Aliquam vitae est non elit blandit dictum."],
+                           ], columns=["Atribut/fitur", "Deskripsi"])
+st.subheader("Deskripsi")
+st.table(attr_desc)
 
